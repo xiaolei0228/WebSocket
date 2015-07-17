12fd88eb852089bb1d869e04190293b7c93c9b29
郝晓雷 <xiaolei-0228@163.com>
2015-07-17 19:54:29 +0800

    WebSocket

--- websocket/src/main/java/com/ehome/websocket/config/WebSocketConfig.java
+++ websocket/src/main/java/com/ehome/websocket/config/WebSocketConfig.java
@@ -10,13 +10,14 @@ import java.util.Set;
  *
  * @author: 郝晓雷
  * @date: 2015-07-12 15:40
- * @desc: websocket配置
+ * @desc: websocket配置，只要实现ServerApplicationConfig接口就会在窗口启动的时候执行下面的getAnnotatedEndpointClasses(...) 或getEndpointConfigs(...)
  */
 public class WebSocketConfig implements ServerApplicationConfig {
 
     /**
      * 注解方式来获取websocket服务
-     * @param set
+     *
+     * @param set 注解有@ServerEndpoint的类的集合
      */
     public Set<Class<?>> getAnnotatedEndpointClasses(Set<Class<?>> set) {
         System.out.println("正在扫描websocket服务..." + set.size());
@@ -25,6 +26,7 @@ public class WebSocketConfig implements ServerApplicationConfig {
 
     /**
      * 接口方式来获取websocket服务，(作用和上面的注解方式一样)
+     *
      * @param set
      */
     public Set<ServerEndpointConfig> getEndpointConfigs(Set<Class<? extends Endpoint>> set) {
---
+++ websocket/src/main/java/com/ehome/websocket/endpoint/ChatSocket.java
@@ -0,0 +1,65 @@
+package com.ehome.websocket.endpoint;
+
+import javax.websocket.CloseReason;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.Set;
+import java.util.logging.Logger;
+
+/**
+ * WebSocket
+ *
+ * @author: 郝晓雷
+ * @date: 2015-07-12 15:25
+ * @desc: 广播
+ */
+@ServerEndpoint("/echo")
+public class ChatSocket {
+    private Logger logger = Logger.getLogger(this.getClass().getName());
+
+    static Map<String,Session> sessionMap = new Hashtable<>();
+
+
+    @OnOpen
+    public void open(Session session) {
+        sessionMap.put(session.getId(), session);
+    }
+
+    @OnMessage
+    public void message(Session session, String msg) {
+        msg = "sessionId" + session.getId() + "说：" + msg;
+        broadcast("message", msg);
+    }
+
+    @OnClose
+    public void close(Session session, CloseReason closeReason) {
+        sessionMap.remove(session.getId());
+        logger.info(String.format("Session %s closed because of %s", session.getId(), closeReason));
+
+    }
+
+    @OnError
+    public void error(Session session, java.lang.Throwable throwable) {
+        sessionMap.remove(session.getId());
+        System.err.println("session " + session.getId() + " error:" + throwable);
+    }
+
+
+    private static void broadcast(String type, String message) {
+        Set<Map.Entry<String, Session>> set = sessionMap.entrySet();
+        for (Map.Entry<String, Session> session : set) {
+            try {
+                session.getValue().getBasicRemote().sendText("{type:'" + type + "',text:'" + message + "'}");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
